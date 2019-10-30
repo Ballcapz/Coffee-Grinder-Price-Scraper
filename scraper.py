4509bbc31d6b30b462c49c0e405af84e14123e00
Zach Johnson <zjohnson@omnitech-inc.com>
2019-10-30 14:51:06 -0500

    Updated to check amazon price too

--- scraper.py
+++ scraper.py
@@ -7,15 +7,16 @@ load_dotenv()
 
 EMAIL_PWD = os.getenv("EMAIL_APP_CREDS")
 
-URL = 'https://www.seattlecoffeegear.com/baratza-sette-30-grinder'
+SCG_URL = 'https://www.seattlecoffeegear.com/baratza-sette-30-grinder'
+AMZN_URL = 'https://www.amazon.com/Baratza-Sette-30-Conical-Grinder/dp/B075G11F9N/ref=sr_1_2?crid=2Z502RIPRQ7E9&keywords=baratza+sette+30&qid=1572464591&sprefix=baratza+se%2Caps%2C188&sr=8-2'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
 
 email_sender = 'zach.dev.full.send@gmail.com'
 receivers = ['zlg23johnson@gmail.com']
 
-def check_price():
-    page = requests.get(URL, headers=headers)
+def check_scg_coffee_price():
+    page = requests.get(SCG_URL, headers=headers)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -23,20 +24,35 @@ def check_price():
     converted_price = float(price[1:5])
 
     if (converted_price <= 226.0):
-        send_mail()
+        bara_subject = 'Baratza Grinder Price Drop!'
+        bara_body = f'Check it out here {SCG_URL}'
+        send_mail(bara_subject, bara_body)
     else:
-        print(converted_price)
+        print(f'SCG Price is currently {converted_price}')
 
-def send_mail():
+def check_amzn_coffee_price():
+    page = requests.get(AMZN_URL, headers=headers)
+
+    amzn_soup = BeautifulSoup(page.content, 'html.parser')
+    real_amzn_soup = BeautifulSoup(amzn_soup.prettify(), 'html.parser')
+
+    amzn_price = real_amzn_soup.find(id="priceblock_ourprice").get_text()
+    amzn_converted_price = float(amzn_price[1:5])
+
+    if (amzn_converted_price > 226.0):
+        amzn_subject = 'Baratza Grinder Price Drop!'
+        amzn_body = f'Check it out here {AMZN_URL}'
+        send_mail(amzn_subject, amzn_body)
+    else:
+        print(f'Amazon price is currently {amzn_converted_price}')
+
+def send_mail(subject, body):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
     server.login(email_sender, EMAIL_PWD)
-
-    subject = 'Baratza Grinder Price Down'
-    body = 'Check the link ' + URL
 
     message = f"Subject: {subject}\n\n{body}"
 
@@ -49,4 +65,5 @@ def send_mail():
     server.quit()
 
 
-check_price()
+check_scg_coffee_price()
+check_amzn_coffee_price()
